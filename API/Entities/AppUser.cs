1c39bc77162ca560948011b195c9b1c3491f9ae2
JanmeshPatel18 <janmeshpatel2003@gmail.com>
2024-12-20 16:30:02 +0530

    Section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -7,4 +7,8 @@ public class AppUser
     public int Id { get; set; }
 
     public required string  UserName { get; set; } 
+
+    public required byte[] HashPassword { get; set; }
+
+    public required byte[] HashSalt { get; set; }
 }
